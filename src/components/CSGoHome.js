e43cac7c43cebb287ec3f8e52e7a01e4293fba70
Alex <acrane2@luc.edu>
2020-11-25 16:12:53 -0600

    CSGo page and HomePage.js

---
+++ src/components/CSGoHome.js
@@ -0,0 +1,47 @@
+<script src="http://localhost:8097"></script>
+import React, { Component } from 'react';
+import { View, Text, Image, StyleSheet, Button, ScrollView, TouchableOpacity } from "react-native";
+
+const CSGoHome = () => {
+
+    const styles = StyleSheet.create({
+        logo: {
+            width: 400,
+            resizeMode: 'contain',
+            // height: 50,
+            // display: "flex",
+        },
+        buttons: { width: 300, marginTop: 10, },
+
+    });
+    return (
+        <View style={{ flex: 1, backgroundColor: 'black', }}>
+            <View style={{
+                flex: 1, borderTopColor: "#DE9B35", borderBottomColor: "#DE9B35", borderWidth: 4, justifyContent: 'center',
+            }}>
+                <Image style={styles.logo} source={require("TeamUpRN/src/images/csgo.png")} />
+            </View>
+            <View style={{ flex: 2, display: 'flex', justifyContent: 'center', alignItems: 'center', }}>
+
+                <View style={styles.buttons}>
+                    <Button title="Find Players" color="#DE9835" />
+                </View>
+                <View style={styles.buttons}>
+                    <Button title="Profile" color="#DE9835" />
+                </View>
+                {/* <TouchableOpacity style={{ height: 100, flex: 1, backgroundColor: 'red' , }}><Text>My buttons</Text></TouchableOpacity> */}
+
+            </View>
+            <View style={{ alignItems: 'center', }}>
+                <View style={styles.buttons}>
+                    <Button title="Sign Out" color="#DE9835" />
+                </View>
+
+            </View>
+        </View>
+
+    );
+
+}
+
+export default CSGoHome;
