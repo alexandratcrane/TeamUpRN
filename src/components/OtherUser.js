9166e282173599549cab7ae5f665d284c256258b
Alex <acrane2@luc.edu>
2020-11-26 11:17:23 -0600

    added profile image to the OtherUser component. will make it look nice later

--- src/components/OtherUser.js
+++ src/components/OtherUser.js
@@ -5,8 +5,9 @@ const styles = StyleSheet.create({
     mainDiv: { height: 100, width: 350, backgroundColor: 'blue', borderRadius: 10, flexDirection: 'row', },
     userTxt: { color: 'white', },
     profileDiv: { flex: 1, },
-    infoDiv: { flex: 1, },
-    activeDiv: { flex: 1, },
+    profile: { resizeMode: 'contain', width: 100, height: 100, },
+    infoDiv: { flex: 1, width: 100, },
+    activeDiv: { flex: 1, width: 100, },
 });
 // each other user will be in the queue page 
 const OtherUser = () => {
@@ -15,7 +16,7 @@ const OtherUser = () => {
         <View style={styles.mainDiv}>
             {/* no source for img yet because it should be based on data */}
             <View style={styles.profileDiv}>
-                <Image alt="profPic" /></View>
+                <Image style={styles.profile} source={require('TeamUpRN/src/images/profile.png')} alt="profPic" /></View>
             <View style={styles.infoDiv}>
                 <Text style={styles.userTxt}>Username:</Text>
                 <Text style={styles.userTxt}>Rank:</Text>
