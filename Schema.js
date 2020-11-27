987324cd3687cb6cf94935bafca56bbc18f3a4b6
Jalan Cruz <42776837+JayCruzer17@users.noreply.github.com>
2020-11-27 15:45:28 -0600

    Update Schema.js

--- Schema.js
+++ Schema.js
@@ -1,15 +1,39 @@
-const mongoose = require("mongoose");
+import { ObjectId } from "bson";
 
-const Schema = mongoose.Schema;
+class Task {
+  /**
+   *
+   * @param {string} name The name of the task
+   * @param {string status The status of the task. Default value is "Open"
+   * @param {ObjectId} id The ObjectId to create this task with
+   */
+  constructor({
+    name,
+    partition,
+    status = Task.STATUS_OPEN,
+    id = new ObjectId(),
+  }) {
+    this._partition = partition;
+    this.name = id;
+	this.mainRole = mainRole;
+    this.firstName = firstName;
+    this.status = status;
+  }
 
-const UserSchema = new Schema({
-	name: String,
-	ign: String,
-	role: String
-});
+  static STATUS_OPEN = "Open";
+  static STATUS_IN_PROGRESS = "InProgress";
+  static STATUS_COMPLETE = "Complete";
+  
+  // TODO: implement schema
+  static schema = {
+  name: "User",
+  properties: {
+    Username: "string", 
+    role: "string", /* main roles can be support, entry fragger, flex */
+	rank: "string",
+    status: "string",
+  },
+  primaryKey: "name",
+};
 
-const User = mongoose.model("user", UserSchema);
-
-//export
-module.exports = User;
-
+export { Task };
