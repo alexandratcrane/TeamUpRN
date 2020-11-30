987ad4aacaf555847ab9c3970e6578fae380acf8
JackNarowski <jacknarowski@gmail.com>
2020-11-30 12:20:11 -0600

    Added naviagation bits having some remaining issues

--- App.js
+++ App.js
@@ -16,10 +16,17 @@ import Profile from './src/components/profile.js';
 import Edit from './src/components/edit.js';
 import Queue from './src/components/Queue.js';
 
+//npm install @react-navigation/native@5.7.3
+// npm install @react-navigation/stack@5.9.0 @react-native-community/masked-view@0.1.10 react-native-screens@2.10.1 react-native-safe-area-context@3.1.4 react-native-gesture-handler@1.7.0
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 // import { NavigationContainer } from '@react-navigation/native';
 // import { createStackNavigator } from '@react-navigation/stack';
 
 // const Stack = createStackNavigator();
+const Stack = createStackNavigator();
 
 
 import {
@@ -48,8 +55,17 @@ class App extends Component {
       <>
         {/* navigation stuff imported in */}
         {/* <NavigationContainer> */}
-        <StatusBar barStyle="dark-content" />
-        <Queue />
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomePage} />
+                <Stack.Screen name="CSGoHome" component={CSGoHome} />
+                <Stack.Screen name="Profile" component={Profile} />
+                <Stack.Screen name="Edit" component={Edit} />
+                <Stack.Screen name="Queue" component={Queue} />
+            </Stack.Navigator>
+        </NavigationContainer>
+//        <StatusBar barStyle="dark-content" />
+//        <Queue />
 
         {/* <SafeAreaView> */}
 
