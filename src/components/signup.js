85ef99f4fbd40adcd14f1d9ba93afcb29d94f559
Jalan Cruz <42776837+JayCruzer17@users.noreply.github.com>
2020-12-02 12:42:25 -0600

    Update signup.js
    Functional component, put the useState() inside function

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,6 +1,7 @@
 // script is to open the react native debugging tools. keep above react/react native imports
 <script src="http://localhost:8097"></script>
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
+import { useAuth } from '../../providers/AuthProvider.js';
 import { View, Text, Image, StyleSheet, Button, ScrollView, TextInput, KeyboardAvoidingView } from "react-native";
 import TeamUpSVG from 'TeamUpRN/src/images/TeamUp_Logo.svg';
 
@@ -45,48 +46,48 @@ const styles = StyleSheet.create({
     },
 });
 
-const { email, setEmail } = useState("");
-const { password, setPassword } = useState("");
-const { steamId, setSteam } = useState("");
-const { rank, setRank } = useState("");
-const { role, setRole } = useState("");
-const { user, signUp, signIn } = useAuth();
-
-function Signup() {
-    return (
+const Signup = () => {
+	const [ email, setEmail ] = useState("");
+	const [ password, setPassword ] = useState("");
+	const [ steamId, setSteam ] = useState("");
+	const [ rank, setRank ] = useState("");
+	const [ role, setRole ] = useState("");
+	const [ user, signUp, signIn ] = useAuth();
+        return(
         <>
             <View style={{ flex: 1, backgroundColor: 'black', }}>
-                <View style={{ flex: 1, }} style={styles.container}>
-                    <TeamUpSVG width={200} height={250} />
+                <View style={{ flex: 1,}} style={styles.container}>
+                    <TeamUpSVG width={200} height={250}/>
                 </View>
-                <View style={{ flex: 3, }} style={styles.container}>
+                <View style={{ flex: 3,}} style={styles.container}>
                     <View>
                         <Text style={styles.text}>Sign Up</Text>
                     </View>
                     <View style={styles.container}>
-                        <TextInput style={styles.form}
-                            placeholder="email address"
-                            onChangeText={(setEmail)} value={email} />
-                        <TextInput style={styles.form}
-                            placeholder="steam username"
-                            onChangeText={(setSteam)} value={steamID} />
-                        <TextInput style={styles.form}
-                            placeholder="Password"
-                            onChangeText={(text) => setPassword(text)} />
-                        <TextInput style={styles.form}
-                            placeholder="Rank"
-                            onChangeText={(setRank)} value={rank} />
-                        <TextInput style={styles.form}
-                            placeholder="Role"
-                            onChangeText={(setRole)} value={role} />
+                    <TextInput style={styles.form}
+                        placeholder="email address" 
+						onChangeText={(setEmail)} value={email} />
+		    <TextInput style={styles.form}
+                        placeholder="steam username" 
+						onChangeText={(setSteam)} value={steamID} />
+		    <TextInput style={styles.form}
+                        placeholder="Password" 
+						onChangeText={(text) => setPassword(text)} />
+                    <TextInput style={styles.form}
+                        placeholder="Rank" 
+						onChangeText={(setRank)} value={rank} />
+                    <TextInput style={styles.form}
+                        placeholder="Role" 
+						onChangeText={(setRole)} value={role} />   
                     </View>
                     <View style={styles.buttons}>
-                        <Button color="#DE9B35" style={styles.buttons} title="Sign Up" onPress={() => this.props.navigation.navigate('CSGoHome')} title="sign up" />
+                        <Button color="#DE9B35" style={styles.buttons} title="Sign Up" onPress={() =>this.props.navigation.navigate('CSGoHome')}title="sign up"/>
                     </View>
                 </View>
             </View>
-        </>
-    );
-};
+            </>
+        );
+        
+    }
 
 export default Signup;
